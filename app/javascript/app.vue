2e4a03dee26b167105f7f1599b40cf8ff8bd8a5e
kohei.yamamoto <kohei.yamamoto1012@gmail.com>
2021-11-30 18:56:35 +0900

    feat: トップページへの実装_#14

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,11 +1,20 @@
 <template>
-  <div>
+  <div class="d-flex flex-column min-vh-100">
+    <TheHeader class="mb-auto" />
     <router-view />
+    <TheFooter class="mt-auto" />
   </div>
 </template>
 
 <script>
+import TheHeader from './components/TheHeader.vue'
+import TheFooter from './components/TheFooter.vue'
+
 export default {
+  components: {
+    TheHeader,
+    TheFooter
+  },
   data: function () {
     return {
     }
