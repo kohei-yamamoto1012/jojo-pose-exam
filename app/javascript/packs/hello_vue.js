6424d91376c68fd276dc5c2414f92e70128e712d
kohei.yamamoto <kohei.yamamoto1012@gmail.com>
2021-12-01 13:28:11 +0900

    feat: axiosの導入_#16

--- app/javascript/packs/hello_vue.js
+++ app/javascript/packs/hello_vue.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import App from '../app.vue'
+import axios from '../plugins/axios'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import router from './router.js'
 
 Vue.config.productionTip = false
+Vue.prototype.$axios = axios
 
 document.addEventListener('DOMContentLoaded', () => {
 	const app = new Vue({
