48f733cef5a9314ec2c6dded98ef11567906995b
kohei.yamamoto <kohei.yamamoto1012@gmail.com>
2021-12-05 21:56:39 +0900

    feat: vue-persistedstateの導入。選択された検定情報の保持_#18

--- app/javascript/store/store.js
+++ app/javascript/store/store.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import createPersistedState from 'vuex-persistedstate'
 import ExamsModule from './modules/exams'
 
 Vue.use(Vuex)
@@ -7,5 +8,14 @@ Vue.use(Vuex)
 export default new Vuex.Store({
   modules: {
     exams: ExamsModule
-  }
+  },
+  plugins: [createPersistedState(
+    {
+      key: 'jojoPoseExam',
+      paths:[
+        'exams.selectedExam'
+      ],
+      storage: window.localStorage
+    }
+  )]
 })
