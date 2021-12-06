1836da95b666b0bc77330677852768f27f7110f1
kohei.yamamoto <kohei.yamamoto1012@gmail.com>
2021-12-06 21:53:50 +0900

    feat: 検定詳細ページを実装_#18

--- app/javascript/pages/exam/index.vue
+++ app/javascript/pages/exam/index.vue
@@ -1,9 +1,38 @@
 <template>
-  <div class="py-4 container">
-    <div class="text-center">
+  <div class="container">
+
+    <div class="mt-4">
       <h2>{{ selectedExam.title }}検定</h2>
+      <img class="img-fluid border" :src="getImagePath(selectedExam.title)" />
+      <p>{{ selectedExam.description }}</p>
     </div>
- 
+
+    <div v-for="(check_item, index) in selectedExamCheckItems" :key="check_item.id">
+      <div class="border py-1 my-2" >
+        <div class="pl-2 py-1">
+          Point: {{ index + 1 }} {{ check_item.content }}
+        </div>
+      </div>
+    </div>
+
+    <div class="form-group mt-4">
+      <label for="jojo-pose-image" class="d-block">ジョジョ立ち画像を添付</label>
+      <input
+        id="jojo-pose-image"
+        type="file"
+        class="form-control-file"
+      >
+      <div class="text-center my-4">
+        <button
+          type="submit"
+          class="btn btn-secondary"
+          @click="takeExam()"
+        >
+        受検するッ！
+        </button>
+      </div>
+    </div>
+
   </div>
 </template>
 
@@ -11,12 +40,34 @@
 import { mapGetters, mapActions, mapMutations } from 'vuex'
 
 export default {
+  created(){
+    this.getCheckItems()
+  },
   data: function () {
     return {
+      check_items: []
     }
   },
   computed: {
-    ...mapGetters('exams', ['selectedExam'])
+    ...mapGetters('exams', ['selectedExam']),
+    selectedExamCheckItems(){
+      return this.check_items.filter( check_item => check_item.exam_id === this.selectedExam.id )
+    }
+  },
+  methods: {
+    getCheckItems(){
+      this.$axios.get('/api/check_items')
+        .then(res => {
+          this.check_items = res.data
+        })
+        .catch(err => console.log(err.status))
+    },
+    getImagePath(title){
+      return require(`../../../assets/images/${title}.png`)
+    },
+    takeExam(){
+      console.log("受検ッ")
+    }
   }
 
 }
