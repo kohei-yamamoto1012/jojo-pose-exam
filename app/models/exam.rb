4b6a75bdcdd2e5faa41e4e8237a888c1ab9a738e
kohei.yamamoto <kohei.yamamoto1012@gmail.com>
2021-12-09 22:30:11 +0900

    fix: dependentのタイポ修正_#24

--- app/models/exam.rb
+++ app/models/exam.rb
@@ -1,6 +1,6 @@
 class Exam < ApplicationRecord
   has_many :check_items, dependent: :destroy
-  has_many :exam_results, default: :destroy
+  has_many :exam_results, dependent: :destroy
 
   validates :title, presence: true
   validates :path, presence: true
