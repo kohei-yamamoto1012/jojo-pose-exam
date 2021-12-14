fd40f0539f8630e140c664d8792a6c1f82dfb048
kohei.yamamoto <kohei.yamamoto1012@gmail.com>
2021-12-14 17:19:28 +0900

    feat: ジョジョ立ち画像のアップロード処理を実装(バックエンド)_#26

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,14 @@ Rails.application.routes.draw do
   namespace :api, format: 'json' do
     resources :exams, only: %i[index]
     resources :check_items, only: %i[index]
-    resources :exam_results, only: %i[create]
+    resources :exam_results, only: %i[create] do
+      post 'upload_image', on: :member
+    end
   end
 
-  get '*path', to: 'home#index'
+  get '*path', to: 'home#index', constraints: lambda { |req|
+    # active_storageに保存したファイルを参照できるようにするために、
+    # 'rails/active_storage'が含まれているurlはリダイレクト対象外にする
+    req.path.exclude? 'rails/active_storage'
+  }
 end
