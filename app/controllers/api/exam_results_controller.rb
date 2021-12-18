0b54d71e19dbf9d163105e35d2a80aea5eb61dcc
kohei.yamamoto <kohei.yamamoto1012@gmail.com>
2021-12-18 23:06:55 +0900

    feat: 検定結果の取得用のAPIを追加_#28

--- app/controllers/api/exam_results_controller.rb
+++ app/controllers/api/exam_results_controller.rb
@@ -1,23 +1,32 @@
 class Api::ExamResultsController < ApplicationController
+  before_action :set_exam_result, only: %i[show upload_image]
+
   def create
-    exam_result = ExamResult.new(exam_result_params)
-    if exam_result.save
-      render json: exam_result
+    @exam_result = ExamResult.new(exam_result_params)
+    if @exam_result.save
+      render json: ExamResultResource.new(@exam_result).serialize
     else
-      render json: exam_result.errors, status: :bad_request
+      render json: @exam_result.errors, status: :bad_request
     end
   end
 
+  def show
+    render json: ExamResultResource.new(@exam_result).serialize
+  end
+
   def upload_image
-    exam_result = ExamResult.find(params[:id])
-    if exam_result.update(upload_image_prams)
-      render json: exam_result, methods: [:upload_image_url]
+    if @exam_result.update(upload_image_prams)
+      render json: ExamResultResource.new(@exam_result).serialize
     else
-      render json: exam_result.errors, status: :bad_request
+      render json: @exam_result.errors, status: :bad_request
     end
   end
 
   private
+
+  def set_exam_result
+    @exam_result = ExamResult.find(params[:id])
+  end
 
   def exam_result_params
     params.require(:exam_result).permit(
