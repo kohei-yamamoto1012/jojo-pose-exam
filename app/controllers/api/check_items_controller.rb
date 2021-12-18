8eacda37320dce2691b8a579fd2be38b76d0500a
kohei.yamamoto <kohei.yamamoto1012@gmail.com>
2021-12-18 23:21:45 +0900

    feat: 確認ポイントモデルのJSONシリアライザを定義_#28

--- app/controllers/api/check_items_controller.rb
+++ app/controllers/api/check_items_controller.rb
@@ -1,6 +1,7 @@
 class Api::CheckItemsController < ApplicationController
   def index
     @check_items = CheckItem.all
-    render json: @check_items
+    # render json: @check_items # ここ修正
+    render json: CheckItemResource.new(@check_items).serialize
   end
 end
---
+++ app/resources/check_item_resource.rb
@@ -0,0 +1,8 @@
+class CheckItemResource
+  include Alba::Resource
+
+  root_key :check_item
+
+  attributes :id, :exam_id, :content
+
+end
