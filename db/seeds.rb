a2e023523075dc4fbe3bd430bd05c935fa1f7fee
kohei.yamamoto <kohei.yamamoto1012@gmail.com>
2021-12-22 15:49:03 +0900

    feat: 初期データのcsv化_#44

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,135 +1,37 @@
-giorno_exam = Exam.new(
-  title: 'ジョルノ立ち',
-  description: '第5部「黄金の風」の第5話「ブチャラティが来る② 」にて、主人公のジョルノ・ジョバーナが行ったジョジョ立ち。ジョルノの回想シーン中で「ギャング・スター」にあこがれるようになったのだ！というナレーションと共に描かれた。'
-)
-giorno_exam.save!
+require 'csv'
 
-CheckItem.create!([
-  {
-    exam: giorno_exam,
-    check_pattern: 1,
-    content: '顎を軽く引き、正面を見据える',
-    allocation: 10
-  },
-  {
-    exam: giorno_exam,
-    check_pattern: 2,
-    content: '右肘を曲げ、右手で胸元を掴む',
-    allocation: 20
-  },
-  {
-    exam: giorno_exam,
-    check_pattern: 3,
-    content: '左手を軽く握り、左腿に添える',
-    allocation: 20
-  },
-  {
-    exam: giorno_exam,
-    check_pattern: 4,
-    content: '足を大きく開く',
-    allocation: 10
-  },
-  {
-    exam: giorno_exam,
-    check_pattern: 5,
-    content: '右膝を真っ直ぐ斜めに伸ばす',
-    allocation: 20
-  },
-  {
-    exam: giorno_exam,
-    check_pattern: 6,
-    content: '左膝を軽く曲げ、踵を浮かす',
-    allocation: 20
-  }
-])
+CSV.foreach('db/exams.csv', headers: true) do |row|
+  exam = Exam.new(
+    title: row['title'],
+    description: row['description']
+  )
+  CSV.foreach('db/check_items.csv', headers: true) do |row|
+    if row['exam_title'] == exam.title
+      exam.check_items << CheckItem.create!(
+        exam: exam,
+        check_pattern: row['check_pattern'].to_i,
+        content: row['content'],
+        allocation: row['allocation']
+      )
+    end
+  end
+end
 
-Keypoint.create!([
-  { name: 'nose' },
-  { name: 'left_eye' },
-  { name: 'right_eye' },
-  { name: 'left_ear' },
-  { name: 'right_ear' },
-  { name: 'left_shoulder' },
-  { name: 'right_shoulder' },
-  { name: 'left_elbow' },
-  { name: 'right_elbow' },
-  { name: 'left_wrist' },
-  { name: 'right_wrist' },
-  { name: 'left_hip' },
-  { name: 'right_hip' },
-  { name: 'left_knee' },
-  { name: 'right_knee' },
-  { name: 'left_ankle' },
-  { name: 'right_ankle' }
-])
+CSV.foreach('db/keypoints.csv', headers: true) do |row|
+  Keypoint.create!(
+    name: row['name']
+  )
+end
 
-score_border_40 = ScoreBorder.new(
-  score: 40
-)
-score_border_40.save!
+CSV.foreach('db/score_borders.csv', headers: true) do |row|
+  ScoreBorder.create!(
+    score: row['score']
+  )
+end
 
-score_border_60 = ScoreBorder.new(
-  score: 60
-)
-score_border_60.save!
-
-score_border_99 = ScoreBorder.new(
-  score: 99
-)
-score_border_99.save!
-
-score_border_100 = ScoreBorder.new(
-  score: 100
-)
-score_border_100.save!
-
-ExamResultComment.create!([
-  {
-    content: 'やれやれだぜ',
-    score_border: score_border_40
-  },
-  {
-    content: 'あァァァんまりだァァアァ',
-    score_border: score_border_40
-  },
-  {
-    content: '何をするだァー！',
-    score_border: score_border_40
-  },
-  {
-    content: '貧弱！貧弱ゥ！',
-    score_border: score_border_60
-  },
-  {
-    content: 'そう...一味違うのね',
-    score_border: score_border_60
-  },
-  {
-    content: 'ゴゴゴゴゴ',
-    score_border: score_border_60
-  },
-  {
-    content: 'ブラボー！おお…ブラボー！！',
-    score_border: score_border_99
-  },
-  {
-    content: 'ベネ',
-    score_border: score_border_99
-  },
-  {
-    content: 'メメタァ',
-    score_border: score_border_99
-  },
-  {
-    content: 'グレートですよこいつはァ',
-    score_border: score_border_100
-  },
-  {
-    content: 'きさまこのジョジョ立ちやり込んでいるなッ！',
-    score_border: score_border_100
-  },
-  {
-    content: 'ふるえるぞハート！燃えるきるほどヒート！！',
-    score_border: score_border_100
-  }
-])
+CSV.foreach('db/exam_result_comments.csv', headers: true) do |row|
+  ExamResultComment.create!(
+    content: row['content'],
+    score_border: ScoreBorder.find_by(score: row['score_border'])
+  )
+end
