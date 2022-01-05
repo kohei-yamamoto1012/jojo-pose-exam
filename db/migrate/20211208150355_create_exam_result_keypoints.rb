12c723b47630f315fccad204dff99265d9b38022
kohei.yamamoto <kohei.yamamoto1012@gmail.com>
2022-01-05 14:27:58 +0900

    feat: exam_resultテーブルの主キーをuuidに変更_#57

--- db/migrate/20211208150355_create_exam_result_keypoints.rb
+++ db/migrate/20211208150355_create_exam_result_keypoints.rb
@@ -1,7 +1,7 @@
 class CreateExamResultKeypoints < ActiveRecord::Migration[6.1]
   def change
     create_table :exam_result_keypoints do |t|
-      t.belongs_to :exam_result, null: false
+      t.belongs_to :exam_result, type: :uuid, null: false
       t.belongs_to :keypoint, null: false
       t.integer :x_coordinate, null: false
       t.integer :y_coordinate, null: false
--- db/schema.rb
+++ db/schema.rb
@@ -13,19 +13,20 @@
 ActiveRecord::Schema.define(version: 2021_12_20_090401) do
 
   # These are extensions that must be enabled in order to support this database
+  enable_extension "pgcrypto"
   enable_extension "plpgsql"
 
-  create_table "active_storage_attachments", force: :cascade do |t|
+  create_table "active_storage_attachments", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
-    t.bigint "record_id", null: false
-    t.bigint "blob_id", null: false
+    t.uuid "record_id", null: false
+    t.uuid "blob_id", null: false
     t.datetime "created_at", null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
   end
 
-  create_table "active_storage_blobs", force: :cascade do |t|
+  create_table "active_storage_blobs", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "key", null: false
     t.string "filename", null: false
     t.string "content_type"
@@ -37,14 +38,14 @@ ActiveRecord::Schema.define(version: 2021_12_20_090401) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
-  create_table "active_storage_variant_records", force: :cascade do |t|
-    t.bigint "blob_id", null: false
+  create_table "active_storage_variant_records", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.uuid "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
   create_table "check_item_results", force: :cascade do |t|
-    t.bigint "exam_result_id", null: false
+    t.uuid "exam_result_id", null: false
     t.bigint "check_item_id", null: false
     t.boolean "result", default: false, null: false
     t.datetime "created_at", precision: 6, null: false
@@ -72,7 +73,7 @@ ActiveRecord::Schema.define(version: 2021_12_20_090401) do
   end
 
   create_table "exam_result_keypoints", force: :cascade do |t|
-    t.bigint "exam_result_id", null: false
+    t.uuid "exam_result_id", null: false
     t.bigint "keypoint_id", null: false
     t.integer "x_coordinate", null: false
     t.integer "y_coordinate", null: false
@@ -83,7 +84,7 @@ ActiveRecord::Schema.define(version: 2021_12_20_090401) do
     t.index ["keypoint_id"], name: "index_exam_result_keypoints_on_keypoint_id"
   end
 
-  create_table "exam_results", force: :cascade do |t|
+  create_table "exam_results", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.bigint "exam_id", null: false
     t.bigint "exam_result_comment_id", null: false
     t.boolean "privacy_setting", default: true, null: false
