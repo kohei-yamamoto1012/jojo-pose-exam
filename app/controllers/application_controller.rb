99fd4ec4a702443852009ff8715032d9dca97e6b
kohei.yamamoto <kohei.yamamoto1012@gmail.com>
2022-02-05 16:48:08 +0900

    feat: herokuapp.comにアクセスされた場合のリダイレクト処理を追加#122

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
+  before_action :ensure_domain, if: :production?
+  FQDN = 'jojo-pose-exam.jp'
+
+  def ensure_domain
+    return unless /\.herokuapp.com/.match?(request.host)
+
+    port = ":#{request.port}" unless [80, 443].include?(request.port)
+    redirect_to "#{request.protocol}#{FQDN}#{port}#{request.path}", status: :moved_permanently
+  end
+
+  def production?
+    Rails.env.production?
+  end
 end
